485da9b1e13df0a8683ef5a211f7148d9fc88e4a
mochaccin <mochaccindotcl@gmail.com>
2025-01-05 15:15:14 -0300

    added course code to code model

--- internal/types/course.go
+++ internal/types/course.go
@@ -5,6 +5,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Course struct {
 	ID       primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 	Name     string             `json:"name" bson:"name"`
+	Code     string             `json:"code" bson:"code"`
 	Teacher  primitive.ObjectID `json:"teacher" bson:"teacher,omitempty"`
 	Students []Student          `json:"students,omitempty" bson:"students,omitempty"`
 }
