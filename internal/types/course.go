213796150a9230a7340def0e6ad3d29765771886
mochaccin <mochaccindotcl@gmail.com>
2025-01-05 20:36:57 -0300

    added course type

--- internal/types/course.go
+++ internal/types/course.go
@@ -3,9 +3,11 @@ package types
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Course struct {
-	ID       primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	Name     string             `json:"name" bson:"name"`
-	Code     string             `json:"code" bson:"code"`
-	Teacher  primitive.ObjectID `json:"teacher" bson:"teacher,omitempty"`
-	Students []Student          `json:"students,omitempty" bson:"students,omitempty"`
+	ID        primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Name      string             `json:"name" bson:"name"`
+	Code      string             `json:"code" bson:"code"`
+	Type      string             `json:"type" bson:"type"`
+	Teacher   primitive.ObjectID `json:"teacher" bson:"teacher,omitempty"`
+	Students  []Student          `json:"students,omitempty" bson:"students,omitempty"`
+	Schedules []string           `json:"schedules,omitempty" bson:"schedules,omitempty"`
 }
